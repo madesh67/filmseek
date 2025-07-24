3d19401c167711e9116c10a844de5f1b6f4eccbd
madesh67 <madesh1367@gmail.com>
2025-07-24 22:17:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/filmseek/',  // Must match your repository name
   plugins: [react()],
 })
