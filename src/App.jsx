e0aeedea48e6639f2909656d90abe67d6ff9f68a
madesh67 <madesh1367@gmail.com>
2025-07-24 22:06:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { HashRouter as router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Homepage from './Components/Homepage';
 import Movies from './Components/MovieSearch';
 import MovieDetails from './Components/MovieDetails';
 
 export default function App(){
   return (
-    <Router basename="/filmseek">
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/movie-details/:type/:id" element={<MovieDetails />} />
-    </Routes>
-    </BrowserRouter>
+    <Router>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movie-details/:type/:id" element={<MovieDetails />} />
+      </Routes>
+      </BrowserRouter>
     </Router>
   )
 }
