84e2bac8055270d7784f28472686fefacd19dcce
madesh67 <madesh1367@gmail.com>
2025-07-24 22:12:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Components/Homepage';
 import Movies from './Components/MovieSearch';
 import MovieDetails from './Components/MovieDetails';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/filmseek">
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movie-details/:type/:id" element={<MovieDetails />} />
-        {/* Add catch-all route for unmatched paths */}
         <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
     </Router>
