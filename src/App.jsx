fd9242ea4507c7195d05645d9d68fb8aa0cd5185
madesh67 <madesh1367@gmail.com>
2025-07-24 22:24:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Homepage from './Components/Homepage';
-import Movies from './Components/MovieSearch';
+import Movie from './Components/MovieSearch';
 import MovieDetails from './Components/MovieDetails';
 
 export default function App(){
@@ -8,7 +8,7 @@ export default function App(){
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/movies" element={<Movies />} />
+      <Route path="/movies" element={<Movie />} />
       <Route path="/movie-details/:type/:id" element={<MovieDetails />} />
     </Routes>
     </BrowserRouter>
